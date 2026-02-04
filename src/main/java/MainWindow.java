94d06821b1597e895d42fae9ccae8aa624e7ac63
mckennaS26 <mstraup@student-bmhs-la.org>
2026-02-04 10:51:55 -0800

    Initial commit

---
+++ src/main/java/MainWindow.java
@@ -0,0 +1,82 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class MainWindow extends JPanel {
+    //1 initial setup of window
+    //1 Panel is the Canvas where we will draw the Art
+    // extend makes the MainWindow object a drawable object
+    private final int WINDOW_WIDTH = 1920;
+    private final int WINDOW_HEIGHT = 1080;
+
+    private final int NUM_PARTICLES = 100;
+    //Particle p;
+
+    ArrayList<Particle> particles;
+
+    public MainWindow() {
+        setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT)); // starting size of window
+
+
+        particles = new ArrayList<>();
+
+        for(int i = 0; i < NUM_PARTICLES; i++){
+            // min + Maht.random() * (max - min)
+            int x = randInt(0, WINDOW_WIDTH);
+            int y = randInt(0, WINDOW_HEIGHT);
+            int radius = randInt(5, 15);
+            int red = randInt(0, 255);
+            int green = randInt(0, 255);
+            int blue = randInt(0, 255);
+
+            // create a local radius variable, and assign a value btwn 5-15
+            //create RBG values btwn 0-255 to assing a random Color to new Color
+            particles.add(new Particle(x, y, radius, new Color(red, green, blue), WINDOW_WIDTH, WINDOW_HEIGHT));
+
+        }
+        //runs every 16 milliseconds (1000/16 ~ 60fps)
+        Timer timer = new Timer(16, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                update();
+                repaint();
+
+            }
+        });
+        timer.start();
+    }
+
+    public int  randInt(int min, int max) {
+        return (int)(min + Math.random() * (max - min));
+    }
+    public static void main(String[] args){
+        // frame is the window (bar, close button, resizing)
+        JFrame frame = new JFrame ("Title Here");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // actually quit the program when the window closes
+
+        // initial setup of the window
+        MainWindow mainWindow = new MainWindow();
+        frame.setContentPane(mainWindow); // puts our drawable object int the frame
+        frame.pack(); // sizes the frame to match our preferred size
+        frame.setLocationRelativeTo(null); // opens the window in the middle of the screen
+        frame.setVisible(true); // false would hide the window
+
+    }
+
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        for(Particle pTemp : particles) {
+            pTemp.draw(g2);
+        }
+    }
+
+    public void update() {
+       for( Particle pTemp : particles){
+           pTemp.updateParticleAngular();
+           pTemp.updateParticleLinear();
+       }
+    }
+}
---
+++ src/main/java/Particle.java
@@ -0,0 +1,60 @@
+import java.awt.*;
+
+public class Particle {
+    // initialize x, y, color, radius
+    private int x, y, radius, xDirection, xSpeed, yDirection, ySpeed;
+    private Color color;
+
+    private int width, height;
+
+    // variables for circular motion
+    private double angle;
+    private double angularspeed ;
+    private int orbitRadius;
+    private int centerX;
+    private int centerY;
+
+    //initial setup of the particle
+    public Particle(int x, int y, int radius, Color color, int windowWidth, int windowHeight){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+
+        xDirection = 1;
+        yDirection = 1;
+
+        xSpeed = (int)(2 + Math.random() * (5 - 2));
+        ySpeed = (int)(2 + Math.random() * (5 - 2));
+
+        width = windowWidth;
+        height = windowHeight;
+
+        centerX = windowWidth / 2;
+        centerY = windowHeight / 2;
+    }
+
+    public void draw(Graphics2D g){
+        g.setColor(color);
+        g.fillOval(x, y, radius*2, radius*2);
+    }
+
+    public void updateParticleAngular() {
+       angle += angularspeed;
+       x = (int) (centerX + orbitRadius * Math.cos(angle));
+       y = (int) (centerY + orbitRadius * Math.sin(angle));
+    }
+
+   public void updateParticleLinear(){
+       if(Math.random() >= .97 || x + (radius*2) > width){
+            xDirection *= -1;
+        }
+        if(Math.random() >= .98 || y + (radius*2) > height){
+            yDirection *= -1;
+        }
+
+        x += xSpeed * xDirection;
+        y += ySpeed * yDirection;
+
+    }
+}
